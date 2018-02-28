aca98d5c75d963815764c1c56b42e6547aa7c87b
Johan Quiroga <quirogacj@utp.edu.co>
2018-02-27 22:54:59 -0500

    Added a dismiss button to dismiss entries from list

--- src/App.js
+++ src/App.js
@@ -28,6 +28,14 @@ class App extends Component {
     this.state = {
       list,
     };
+
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id) {
+    this.setState({
+      list: this.state.list.filter((item) => item.objectID !== id)
+    });
   }
 
   render() {
@@ -41,6 +49,14 @@ class App extends Component {
             <span>{item.author}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
+            <span>
+              <button
+                onClick={() => this.onDismiss(item.objectID)}
+                type="button"
+              >
+                Dismiss
+              </button>
+            </span>
           </div>
         )}
       </div>
