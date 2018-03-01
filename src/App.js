c02c13717488e83473fd684ed355d471d851498b
Johan Quiroga <quirogacj@utp.edu.co>
2018-02-28 21:50:38 -0500

    Basic example of method bindings in components

--- src/App.js
+++ src/App.js
@@ -64,4 +64,33 @@ class App extends Component {
   }
 }
 
-export default App;
+class ExplainBindingsComponent extends Component {
+  constructor() {
+    super();
+
+    this.onClickMe = this.onClickMe.bind(this);
+  }
+
+  onClickMe() {
+    console.log(this);
+  }
+
+  // OR
+
+  // onClickMe = () => {
+  //   console.log(this);
+  // }
+
+  render() {
+    return (
+      <button
+        onClick={this.onClickMe}
+        type="button"
+      >
+        Click Me
+      </button>
+    );
+  }
+}
+
+export default ExplainBindingsComponent;
